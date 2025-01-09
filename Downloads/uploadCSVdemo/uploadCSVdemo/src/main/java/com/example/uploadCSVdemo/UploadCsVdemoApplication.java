563f0d24e86ba0e78b6454fe1c7ca545de151b19
Hp <thakre.atul26@gmail.com>
2025-01-09 22:52:10 +0530

    Add my new Project

---
+++ Downloads/uploadCSVdemo/uploadCSVdemo/src/main/java/com/example/uploadCSVdemo/UploadCsVdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.uploadCSVdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadCsVdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadCsVdemoApplication.class, args);
+	}
+
+}
---
+++ Downloads/uploadCSVdemo/uploadCSVdemo/src/test/java/com/example/uploadCSVdemo/UploadCsVdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uploadCSVdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadCsVdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
